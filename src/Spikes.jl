7bbcbc01154c11f0b7dd4df9725d5bb56d501886
Stefano Bettani <bettani@bio.ens.pls.eu>
2021-03-02 15:19:49 +0100

    refractoring fig 3, removing useless crosscor functions

--- src/Spikes.jl
+++ src/Spikes.jl
@@ -16,6 +16,7 @@ include("average.jl")
 
 
 export bin,
+	   deviation,
 	   average,
 	   relative!,
 	   convolve,
--- src/average.jl
+++ src/average.jl
@@ -1,13 +1,20 @@
 using Statistics
 using DataFrames
 
-function average(x::Vector{<:Vector{<:Real}}, df::DataFrame)
+function average(x::Vector, df::DataFrame)
 	average(x, df.lift)
 end
 
-function average(x::Vector{<:Vector{<:Real}}, y::Vector{<:Vector{<:Real}})
+function average(x::Vector, y::Vector{<:Vector{<:Real}})
 	a = cumsum(length.(y))
 	b = pushfirst!(a[1:end-1] .+ 1, 1)
 	c = UnitRange.(b, a) 
 	[mean(x[r]) for r in c]
+end
+
+function deviation(x::Vector, y::Vector{<:Vector{<:Real}})
+	a = cumsum(length.(y))
+	b = pushfirst!(a[1:end-1] .+ 1, 1)
+	c = UnitRange.(b, a) 
+	[std(x[r]) for r in c]
 end
