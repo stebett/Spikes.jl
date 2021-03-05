78d2cd5be95e5ef9f95694f499837cc5221bfb1e
Thinkpad <ginkobab@gmail.com>
2021-03-05 20:39:38 +0100

    fixed runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using SafeTestsets
 
 
 @testset "Spikes.jl" begin
-	@safetestset "bin" begin include("test/bin-test.jl") end
-	@safetestset "average" begin include("test/average-test.jl") end
-	@safetestset "convolve" begin include("test/convolve-test.jl") end
-	@safetestset "cross-correlation" begin include("test/cross-correlation-test.jl") end
+	@safetestset "bin" begin include("bin-test.jl") end
+	@safetestset "average" begin include("average-test.jl") end
+	@safetestset "convolve" begin include("convolve-test.jl") end
+	@safetestset "cross-correlation" begin include("cross-correlation-test.jl") end
 end;
